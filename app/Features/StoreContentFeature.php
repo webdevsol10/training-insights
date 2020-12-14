d0265b85ecd280a760913833ec4ca3c179485de5
ttt <webdevsol6@gmail.com>
2020-12-14 12:33:16 +0200

    init

---
+++ app/Features/StoreContentFeature.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Features;
+
+use Lucid\Units\Feature;
+use Illuminate\Http\Request;
+
+class StoreContentFeature extends Feature
+{
+    public function handle(Request $request)
+    {
+
+    }
+}
---
+++ tests/Features/StoreContentFeatureTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Features;
+
+use Tests\TestCase;
+use App\Features\StoreContentFeature;
+
+class StoreContentFeatureTest extends TestCase
+{
+    public function test_storecontentfeature()
+    {
+        $this->markTestIncomplete();
+    }
+}
