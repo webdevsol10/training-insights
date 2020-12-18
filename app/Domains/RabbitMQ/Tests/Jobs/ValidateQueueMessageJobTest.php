979319c5d66a1e3f53f025bb993f294b51cf6442
Vlad Silchenko <webdevsol10@gmail.com>
2020-12-18 11:47:18 +0200

    validation tests for fail

--- app/Domains/RabbitMQ/Tests/Jobs/ValidateQueueMessageJobTest.php
+++ app/Domains/RabbitMQ/Tests/Jobs/ValidateQueueMessageJobTest.php
@@ -3,11 +3,12 @@
 namespace App\Domains\RabbitMQ\Tests\Jobs;
 
 use App\Domains\RabbitMQ\Jobs\ValidateQueueMessageJob;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class ValidateQueueMessageJobTest extends TestCase
 {
-    public function test_validate_queue_message_job()
+    public function test_validate_success_queue_message_job()
     {
         $rawMessage = unserialize(file_get_contents(base_path('tests/resources/insights.txt')));
 
@@ -15,5 +16,26 @@ class ValidateQueueMessageJobTest extends TestCase
         $valid = $job->handle();
 
         $this->assertTrue($valid);
+    }
+
+    public function test_validate_fails_queue_message_job()
+    {
+        $rawMessage = unserialize(file_get_contents(base_path('tests/resources/insights.txt')));
+
+        foreach (array_keys($rawMessage) as $key) {
+            $tempMessage = $rawMessage;
+            unset($tempMessage[$key]);
+
+            try{
+                $job = new ValidateQueueMessageJob($tempMessage);
+                $job->handle();
+
+                $this->fail("Expected exception for key `$key` not thrown");
+            } catch (ValidationException $e){
+
+            }
+        }
+
+        $this->assertTrue(true);
     }
 }
